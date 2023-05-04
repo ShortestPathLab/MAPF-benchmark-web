b33605a5940f30d08081e3669b5d4c7d98000496
bshen95 <Bojie.Shen@monash.edu>
2023-05-04 17:27:48 +1000

    update package.json for quickly set up

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,12 @@
+
+var url = "";
+if (process.env.NODE_ENV === 'development') {
+    url = 'http://127.0.0.1:8080/api';
+} else {
+    url = '/api';
+}
+
 export const APIConfig = {
-    apiUrl: 'http://127.0.0.1:8080/api',
+    apiUrl: url,
     // apiUrl: '/api',
 };
